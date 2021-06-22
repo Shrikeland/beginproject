7b1f8dfd9baec9ba62fee4d1c2b15eb5dc924def
Shrikeland <apaintree@yandex.ru>
2021-06-22 12:05:56 +0500

    second commit

---
+++ types.js
@@ -0,0 +1,38 @@
+// Типы данных (простые типы):
+// Числа
+
+let number = 13;
+console.log(-4/0);
+console.log('string' * 9);
+
+// Строка
+
+const persone = 'Andrew';
+
+// Логический тип (boolean)
+
+const bool = true;
+
+// null и undefined
+
+console.log(something);
+
+let und;
+console.log(und);
+
+// Типы данных (комплексные/объекты):
+
+const obj = {
+    name: 'John',
+    age: 25,
+    isMarried: false
+};
+
+console.log(obj.name);
+
+
+
+// Массив (частный случай комплексного объекта)
+
+let arr = ['plum.png', 'orange.jpg', 6, 'apple.bmp', {}, []];
+console.log(arr[1]);
